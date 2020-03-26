cabe8969e1cbdeed3b0978c39a28fbfff4db1dd7
Pablo García Egido <pgarciaegido@gmail.com>
2020-03-26 20:32:17 +0100

    initial commit :v:

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+      statsOptions: { source: false }
+    })
+  ]
+};
